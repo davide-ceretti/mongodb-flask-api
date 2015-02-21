7374fa410c5fa435e9c02adb620debfa9af99e57
Davide Ceretti <dav.ceretti@gmail.com>
2015-02-21 22:51:23 +0000

    Add hello_world endpoint

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,18 @@
 # -*- coding: utf-8 -*-
+import unittest
 
-from api.placeholder import hello
+from api import application
 
 
-def test_dummy():
-    assert hello() == 'world'
+class TestApi(unittest.TestCase):
+    def setUp(self):
+        application.app.config['TESTING'] = True
+        self.app = application.app.test_client()
+
+    def test_hello_endpoint(self):
+        response = self.app.get('/api/hello')
+        assert response.status_code == 200
+        assert response.data == b'Hello, world!'
+
+if __name__ == '__main__':
+    unittest.main()
