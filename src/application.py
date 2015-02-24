441c32feef97c52b377efcf781a95549581adf3f
Davide Ceretti <davide.ceretti@hogarthww.com>
2015-02-24 18:41:27 +0000

    Initial resource

--- src/application.py
+++ src/application.py
@@ -1,12 +1,31 @@
 # -*- coding: utf-8 -*-
+import json
 
-from flask import Flask
+from flask import Flask, make_response, request
 from flask.ext.pymongo import PyMongo
 
 app = Flask("api")
 mongo = PyMongo(app)
 
 
-@app.route("/api/hello")
+@app.route("/api/hello", methods=['GET'])
 def hello():
-    return "Hello, world!"
+    return make_response("Hello, world!", 200)
+
+
+@app.route("/api/user", methods=['GET', 'POST'])
+def user():
+    if request.method == 'POST':
+        user_dict = json.loads(request.data.decode('utf-8'))
+        mongo.db.user.insert(user_dict)
+        _patch_dict(user_dict)
+        response = make_response(json.dumps(user_dict), 201)
+        response.headers = {'Content-Type': 'application/json'}
+        return response
+
+
+def _patch_dict(my_dict):
+    _id = my_dict.pop('_id', None)
+    if _id is None:
+        raise ValueError("Can't patch dict without _id key")
+    my_dict['id'] = str(_id)
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,18 +1,48 @@
 # -*- coding: utf-8 -*-
 import unittest
+import json
 
 from api import application
+
+
+def resp_data_to_dict(response, encoding='utf-8'):
+    return json.loads(response.data.decode(encoding))
 
 
 class TestApi(unittest.TestCase):
     def setUp(self):
         application.app.config['TESTING'] = True
-        self.app = application.app.test_client()
+        self.app = application.app
+        self.mongo = application.mongo
+        self.client = self.app.test_client()
 
     def test_hello_endpoint(self):
-        response = self.app.get('/api/hello')
+        response = self.client.get('/api/hello')
         assert response.status_code == 200
         assert response.data == b'Hello, world!'
+
+    def test_post_create_user_returns_201(self):
+        data = json.dumps({'username': 'luke88', 'age': 26})
+        response = self.client.post(
+            '/api/user',
+            data=data,
+            headers={'Content-Type': 'application/json'}
+        )
+        assert response.status_code == 201
+
+    def test_post_create_user_returns_valid_data(self):
+        data = json.dumps({'username': 'luke88', 'age': 27})
+        response = self.client.post(
+            '/api/user',
+            data=data,
+            headers={'Content-Type': 'application/json'}
+        )
+        response_data = resp_data_to_dict(response)
+        assert response_data.pop('username') == 'luke88'
+        assert response_data.pop('age') == 27
+        assert isinstance(response_data.pop('id'), str)
+        assert response_data == {}
+
 
 if __name__ == '__main__':
     unittest.main()
